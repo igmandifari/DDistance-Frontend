a1f6127080969eed142e9348eab3300d8d310b9d
Andi Kurniawan <Andiwann1603@gmail.com>
2023-12-08 14:36:18 +0700

    [FEAT] Setup Color

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary: "#F48300",
+      secondary: "#FFC700",
+      background: "#E8E4E4",
+      bgSecondary: "#DBDBDB",
+      buttonColor: "#F8DD91",
+      buttonText: "background: #9D7F2C",
+    },
+    extend: {},
+  },
+  plugins: [],
+};

cb3024433d0074ed1250a65d0ca5b39e71c92e57
Andi Kurniawan <Andiwann1603@gmail.com>
2023-12-13 09:42:58 +0700

    [FEAT] add fiture dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import MainDashboard from "./pages/MainDashboard";
+
 function App() {
   return (
     <>
-      <h1 className="text-2xl text-buttonColor">Test</h1>
-      <h1 className="text-2xl text-primary">Test</h1>
+      <MainDashboard />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#F48300",
-      secondary: "#FFC700",
-      background: "#E8E4E4",
-      bgSecondary: "#DBDBDB",
-      buttonColor: "#F8DD91",
-      buttonText: "background: #9D7F2C",
+    extend: {
+      colors: {
+        primary: "#F48300",
+        secondary: "#FFC700",
+        background: "#E8E4E4",
+        bgSecondary: "#DBDBDB",
+        buttonColor: "#F8DD91",
+        buttonText: "#9D7F2C",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };

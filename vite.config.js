ec40d5945daac31a9c1c5b782a978f434155a26f
Andi Kurniawan <Andiwann1603@gmail.com>
2024-01-19 21:27:28 +0700

    [FIX] vercel json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const env = loadEnv("all", process.cwd());
+// const env = loadEnv("all", process.cwd());
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +10,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: env.VITE_API_URL,
+        // target: env.VITE_API_URL,
+        target: "http://43.218.88.7:8081",
         changeOrigin: true,
         rewrite: (path) => path.replace("^/api/", ""),
       },
